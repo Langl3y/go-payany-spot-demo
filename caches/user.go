50fc3c6bfcb2f166366808c15f690d1a77972350
Langl3y <hieuhovan954@gmail.com>
2025-10-01 15:55:48 +0700

    Move User cache handlers -> caches

---
+++ caches/user.go
@@ -0,0 +1,34 @@
+package caches
+
+import (
+	"context"
+	"errors"
+	"spot_demo/utils"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func SetUserData(account string, userID int, token string, expireTime int64) {
+	redisClient := utils.NewRedisClient()
+
+	data := map[string]interface{}{
+		"user_id": userID,
+		"token":   token,
+	}
+
+	expiresIn := expireTime - utils.CurrentTimestamp()
+	redisClient.HSet(context.Background(), account, data)
+	redisClient.Expire(context.Background(), account, time.Duration(expiresIn)*time.Second)
+}
+
+func GetUserID(account string, password string) (string, error) {
+	redisClient := utils.NewRedisClient()
+	val, err := redisClient.HGet(context.Background(), account, "user_id").Result()
+
+	if errors.Is(redis.Nil, err) {
+		return "", err
+	} else {
+		return val, nil
+	}
+}
