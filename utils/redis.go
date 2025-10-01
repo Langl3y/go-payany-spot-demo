e07994471863cdd4b34f8b5400489d61a7ce5425
Langl3y <hieuhovan954@gmail.com>
2025-10-01 10:24:58 +0700

    Update redis util to generate 1 redis connector per run only

--- utils/redis.go
+++ utils/redis.go
@@ -2,16 +2,24 @@ package utils
 
 import (
 	"spot_demo/constants"
+	"sync"
 
 	"github.com/redis/go-redis/v9"
 )
 
+var (
+	redisClient *redis.Client
+	once        sync.Once
+)
+
 func NewRedisClient() *redis.Client {
-	client := redis.NewClient(&redis.Options{
-		Addr:     constants.RedisAddr,
-		Password: constants.RedisPassword,
-		DB:       constants.RedisDB,
+	once.Do(func() {
+		redisClient = redis.NewClient(&redis.Options{
+			Addr:     constants.RedisAddr,
+			Password: constants.RedisPassword,
+			DB:       constants.RedisDB,
+		})
 	})
 
-	return client
+	return redisClient
 }
