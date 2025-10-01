fb8db864909f1ac2ec0aeaec020068726a1cf42f
Langl3y <hieuhovan954@gmail.com>
2025-10-01 16:31:28 +0700

    Add spot trading stress test

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
 	"spot_demo/business"
 )
 
+func stressTest() {
+	result, err := business.PutSpotLimit("hieuhovan954@gmail.com", "eriri123")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
+}
+
 func main() {
 	for {
-		result, err := business.PutSpotLimit("hieuhovan954@gmail.com", "eriri123")
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Println(result)
+		go stressTest()
 	}
 }
